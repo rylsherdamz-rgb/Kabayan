4f359412086baba561afe171380aaa7f491bb014
rylsherdamz-rgb <rylsherdamz@gmail.com>
2026-02-27 16:36:05 +0800

    added the function tto finish onBoarding

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,10 @@ interface LandingPageProp {
 }
 
 export default function LandingPage({onBoardingFunction} : LandingPageProp) {
-  return <View className="flex-1">
+  return <View className="flex-1 gap-y-5">
     <Text> this is the loadingFirst for the moment</Text>
+    <Pressable className="" onPress={onBoardingFunction}>
+      Enter
+    </Pressable>
   </View>
 }
