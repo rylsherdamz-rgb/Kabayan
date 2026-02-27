6e6937cc21b4744530dcf791f21e98c684375411
rylsherdamz-rgb <rylsherdamz@gmail.com>
2026-02-27 08:33:00 +0800

    added the mainContent on the root file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,16 @@
 import { Stack } from "expo-router";
+import {SafeAreaProvider} from "react-native-safe-area-context"
+import {GestureHandlerRootView} from "react-native-gesture-handler"
+import Drawer from "expo-router/drawer"
+import {StatusBar} from "react-native"
 import "../global.css"
 
+
 export default function RootLayout() {
-  return <Stack />;
+  return <GestureHandlerRootView style={{flex:1}}>
+    <SafeAreaProvider>
+      <StatusBar />
+      <Drawer />
+    </SafeAreaProvider>
+  </GestureHandlerRootView>
 }
