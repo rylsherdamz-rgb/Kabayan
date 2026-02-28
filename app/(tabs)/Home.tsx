9f756a47a4d2c25c83c6ca95d2fbb30f304afa26
rylsherdamz-rgb <rylsherdamz@gmail.com>
2026-02-28 19:45:08 +0800

    added more component to it to fill in leater

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -1,9 +1,10 @@
-import {View, Text} from "react-native"
-import CustomLoadingSpinner from "@/components/CustomLoadingSpinner"
-import CustomMapComponents from "@/components/CustomMapComponents"
+import CustomMapComponents from "@/components/CustomComponents/CustomMapComponents";
+import { View } from "react-native";
 
 export default function Home() {
-    return <View className="">
-        <CustomMapComponents />
+  return (
+    <View className="">
+      <CustomMapComponents />
     </View>
+  );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,57 +1,62 @@
-import {SafeAreaProvider} from "react-native-safe-area-context"
-import {GestureHandlerRootView} from "react-native-gesture-handler"
-import {Redirect} from "expo-router"
-import {useEffect, useState} from "react"
-import {useSafeAreaInsets} from "react-native-safe-area-context"
-import Drawer from "expo-router/drawer"
-import {StatusBar} from "react-native"
-import "../global.css"
-import useLandingPage from "@/hooks/useLandingPage"
-import Index from "./index"
-
-
+import CustomLoading from "@/components/CustomComponents/CustomLoadingSpinner";
+import useLandingPage from "@/hooks/useLandingPage";
+import { Redirect } from "expo-router";
+import Drawer from "expo-router/drawer";
+import { useEffect, useState } from "react";
+import { StatusBar, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider, useSafeAreaInsets } from "react-native-safe-area-context";
+import "../global.css";
 
 export default function RootLayout() {
-  const {getIsFirstOpened, setIsFirstOpened} = useLandingPage()
-  const [FirstOpened, setFirstOpened] = useState<boolean |  null>(false)
-  const inset =  useSafeAreaInsets()
+  const { getIsFirstOpened, setIsFirstOpened } = useLandingPage();
+  const [FirstOpened, setFirstOpened] = useState<boolean | null>(false);
+  const inset = useSafeAreaInsets();
 
-   useEffect(() => {
-    const hasOpened = getIsFirstOpened() 
-    const LandingPageValue = hasOpened === undefined ? true : !hasOpened
-    setFirstOpened(LandingPageValue)
-   }, [])
+  useEffect(() => {
+    const hasOpened = getIsFirstOpened();
+    const LandingPageValue = hasOpened === undefined ? true : !hasOpened;
+    setFirstOpened(LandingPageValue);
+  }, []);
 
-   // pass this funciton in the landingpage of the onboarding
-   const handleFinishOnBoarding = () => {
-    setIsFirstOpened()
-    setFirstOpened(false)
-   }
+  // pass this funciton in the landingpage of the onboarding
+  const handleFinishOnBoarding = () => {
+    setIsFirstOpened();
+    setFirstOpened(false);
+  };
 
-   if (FirstOpened === null) {
+  if (FirstOpened === null) {
     // show the loading Page
-    return 
-   }
+
+    return (
+      <View className="flex flex-1">
+        <CustomLoading />
+      </View>
+    );
+  }
 
   //  if (FirstOpened) {
   //   // show the onboardingPage
   //   return <Index onBoardingFunction={handleFinishOnBoarding} />
   //  }
 
-
-  return <GestureHandlerRootView style={{flex:1}}>
-    <SafeAreaProvider style={{flex : 1}}  >
-      <StatusBar />
-      <Drawer screenOptions={{headerShown: false,
-      }}>
-        <Drawer.Screen name="/(tabs)/Home" options={{drawerLabel : "Home"}} />
-        <Drawer.Screen name="index" options={{drawerItemStyle : { display: 'none'}}}/>
-        {/* <Drawer.Screen name="other-stacks" options={{drawerItemStyle : { display: 'none'}}}/> */}
-      </Drawer>
-
-{FirstOpened === false && <Redirect href={"/(tabs)/Home"}  />}
-
-
-    </SafeAreaProvider>
-  </GestureHandlerRootView>
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider style={{ flex: 1 }}>
+        <StatusBar />
+        <Drawer screenOptions={{ headerShown: false }}>
+          <Drawer.Screen
+            name="/(tabs)/Home"
+            options={{ drawerLabel: "Home" }}
+          />
+          <Drawer.Screen
+            name="index"
+            options={{ drawerItemStyle: { display: "none" } }}
+          />
+          {/* <Drawer.Screen name="other-stacks" options={{drawerItemStyle : { display: 'none'}}}/> */}
+        </Drawer>
+        {FirstOpened === false && <Redirect href={"/(tabs)/Home"} />}
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
 }
