6ef241b431705595f896d63877b4c00e973fb93e
rylsherdamz-rgb <rylsherdamz@gmail.com>
2026-03-09 17:36:15 +0800

    created customModal for login pop

--- app/(tabs)/message.tsx
+++ app/(tabs)/message.tsx
@@ -190,24 +190,6 @@ export default function Inbox() {
         />
       )}
 
-      <Modal visible={authModalVisible} transparent animationType="slide" onRequestClose={() => setAuthModalVisible(false)}>
-        <KeyboardAvoidingView className="" behavior={Platform.OS === "ios" ? "padding" : "height"}>
-          <View className="flex-1 bg-black/50 justify-center px-4">
-            <View className="bg-white rounded-3xl p-6">
-              <Text className="text-xl font-black text-slate-900 mb-3">Create an account</Text>
-              <Text className="text-slate-500 mb-4">Sign in to access your messages.</Text>
-              <View className="flex flex-row  justify-around ">
-             <TouchableOpacity  onPress={() => router.push("/(ProtectedRoutes)/AuthenticationPage")} className="mt-3 rounded-2xl px-5 py-2  bg-blue-600 text-white items-center">
-                <Text className="text-black text-sm">Create Account</Text>
-              </TouchableOpacity>
-              <TouchableOpacity  onPress={() => setAuthModalVisible(false)} className="mt-3 rounded-2xl px-5 py-2  border items-center">
-                <Text className="text-slate-500 text-sm">Maybe later</Text>
-              </TouchableOpacity>
-              </View>
-            </View>
-            </View>
-        </KeyboardAvoidingView>
-      </Modal>
     </KeyboardAvoidingView>
   );
 }
--- components/CustomComponents/CustomModalComponent.tsx
+++ components/CustomComponents/CustomModalComponent.tsx
@@ -1,24 +1,30 @@
-import {Modal, View, Pressable } from "react-native"
+import { router } from "expo-router";
+import { Modal, Text, KeyboardAvoidingView, Platform, View, TouchableOpacity } from "react-native";
 
 
-interface CustomModalComponentProp {
-    visible : boolean
-    setVisible : React.Dispatch<React.SetStateAction<boolean>>
+interface CustomModalProps {
+  authModalVisible : boolean
+  setAuthModalVisible : React.Dispatch<React.SetStateAction<boolean>>
 }
-export default function CustomModalComponent({visible, setVisible} : CustomModalComponentProp) {
-    return <Modal
-    animationType="slide" 
-    visible={visible}
-    onRequestClose={() => setVisible(!visible)} 
-    >
-    <View className="flex flex-1">
-        <View>
 
-        </View>
-        <Pressable>
+export default function CustomModal({authModalVisible, setAuthModalVisible} : CustomModalProps) {
+    return <Modal visible={authModalVisible} transparent animationType="slide" onRequestClose={() => setAuthModalVisible(false)}>
+        <KeyboardAvoidingView className="" behavior={Platform.OS === "ios" ? "padding" : "height"}>
+          <View className="flex-1 bg-black/50 justify-center px-4">
+            <View className="bg-white rounded-3xl p-6">
+              <Text className="text-xl font-black text-slate-900 mb-3">Create an account</Text>
+              <Text className="text-slate-500 mb-4">Sign in to access your messages.</Text>
+              <View className="flex flex-row  justify-around ">
+             <TouchableOpacity  onPress={() => router.push("/(ProtectedRoutes)/AuthenticationPage")} className="mt-3 rounded-2xl px-5 py-2  bg-blue-600 text-white items-center">
+                <Text className="text-black text-sm">Create Account</Text>
+              </TouchableOpacity>
+              <TouchableOpacity  onPress={() => setAuthModalVisible(false)} className="mt-3 rounded-2xl px-5 py-2  border items-center">
+                <Text className="text-slate-500 text-sm">Maybe later</Text>
+              </TouchableOpacity>
+              </View>
+            </View>
+            </View>
+        </KeyboardAvoidingView>
+      </Modal>
 
-        </Pressable>
-    </View>
-
-    </Modal>
 }
